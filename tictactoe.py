47325bc9b4faf7938ecd5e704a6937b9a27477c5
Oliver Chang <oychang1@gmail.com>
2012-04-16 22:40:07 -0400

    primitive prontBoard(). the for loops are wack, but works slightly

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,39 @@
 
 from random import randrange
+def printBoard(possStrings):
+	# !!!! make sure possStrings is formatted to correctly use this function
+	# has to be in form which is top row (LT, CT, RT) -> center -> bottom row
+	# print the coordinates of blanks
+	print "----------------------------------------" # first line
+	
+	# start working on topRow
+	index = 1
+	for x in range(3): # do these instructions 3 times; once for each row
+		left = " " # reset these values to blank and add a space to make blank and filled lines have the same length
+		center = " " 
+		right = " "
+		for z in range(3): # for every column in the working row, do this
+			currEval = possStrings[index] # starts at possStrings[1] = a string that describes the owner: "", "X", or "O"
+			if currEval == "": # if there is no owner
+				index += 2 # then move to the next position's ownership
+			elif currEval != "": # if there is an owner
+				if index == (1 or 7 or 13): # if the left block
+					left = currEval
+				elif index == (3 or 9 or 15): # if center block
+					center = currEval
+				elif index == (5 or 11 or 17): # if right block
+					right = currEval
+				index += 2
+			print "|     %s     |     %s     |     %s     |" % (left,center,right)		
+	print "----------------------------------------" # last line	
 
 print "Tic-Tac-Toe Program"
 print "To play, take turns playing the coordinates below"
 
 
-print "|	LT	|	CT	|	RT	|"
-print "|	LC	|	CC	|	RC	|"
-print "|	LB	|	CB	|	RB	|"
+print "|     LT     |     CT     |     RT     |" # 5 spaces
+print "|     LC     |     CC     |     RC     |"
+print "|     LB     |     CB     |     RB     |"
 
 possMoves = ["LT","CT","RT","LC","CC","RC","LB","CB","RB"]
 possStrings = ["LT","","CT","","RT","","LC","","CC","","RC","","LB","","CB","","RB",""]
@@ -29,16 +55,17 @@ while turn <= 9:
 		
 	doMove = (raw_input(prompt)).upper() # gets a string as an input and converts to upeprcase to match possMoves
 	if doMove in possMoves:
-		possStrings[(possStrings.index(doMove) + 1)] = currPlayer
+		possStrings[(possStrings.index(doMove) + 1)] = currPlayer # get the index of the last move, puts the player as the owner of that tile in the next item in possStrings
 		possMoves.remove(doMove)
+		printBoard(possStrings)
 		turn += 1
 		#print possMoves
 		#print possStrings
 	else:
 		print "invalid input/move"
 
-def printBoard()
-	pass
+
+		
 		
 def checkWin():
 	pass
