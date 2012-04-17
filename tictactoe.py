2332ea9f337efefc56437dacc4227addf0b8f5ad
Oliver Chang <oychang1@gmail.com>
2012-04-16 20:59:21 -0400

    core function works. todo: fill out next functions and change initial board showing to use printBoard()

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,8 @@ print "|	LT	|	CT	|	RT	|"
 print "|	LC	|	CC	|	RC	|"
 print "|	LB	|	CB	|	RB	|"
 
-possMoves = [["LT",""],["CT",""],["RT",""],["LC",""],["CC",""],["RC",""],["LB",""],["CB",""],"RB",""]]
+possMoves = ["LT","CT","RT","LC","CC","RC","LB","CB","RB"]
+possStrings = ["LT","","CT","","RT","","LC","","CC","","RC","","LB","","CB","","RB",""]
 
 # initial turn
 # imported from random module -- gives 1 or 2 pseudorandomly
@@ -18,46 +19,29 @@ turn = randrange(1,3) # 1 = x first, 2 = o first
 
 #def input(possMoves): # takes a list of possible moves
 while turn <= 9:
+	# chooses which prompt to show; only changes on successful move
 	if (turn % 2) == 1:  # if turn is even
-		prompt = "x's turn: "
-	else:
-		prompt = "y's turn: "
+		prompt = "X's turn: "
+		currPlayer = "X"
+	else: # effectively, when turn is odd
+		prompt = "O's turn: "
+		currPlayer = "O"
 		
-	doMove = raw_input(prompt)
+	doMove = (raw_input(prompt)).upper() # gets a string as an input and converts to upeprcase to match possMoves
 	if doMove in possMoves:
-		#parser1(doMove)	
-		print doMove
+		possStrings[(possStrings.index(doMove) + 1)] = currPlayer
 		possMoves.remove(doMove)
 		turn += 1
+		#print possMoves
+		#print possStrings
 	else:
 		print "invalid input/move"
 
-def parser1(x,topList,centerList,bottomList):
-	x = list(x) # from http://mail.python.org/pipermail/tutor/2005-August/040892.html
-	if x[0] == "L":
-		parser2(x[1],0,topList)
-	elif x[0] == "C":
-		parser2(x[1],1,centerList)
-	elif x[0] == "R":
-		parser2(x[1],2,bottomList)
-	else:
-		print "error in parsing"
-	
-def parser2(toParse,otherHalf,moveList):
-	if toParse == "T":
-		if otherHalf == 0: # LT
-			pass
-		elif otherHalf == 1: # CT
-			pass
-		elif otherHalf == 2: # RT
-			pass
-	elif toParse == "C":
-		pass
-	elif toParse == "B":
-		pass
-
-
-
+def printBoard()
+	pass
+		
+def checkWin():
+	pass
 
 
 
