cf502d98edb21093c257dc43fc32b6fffe078018
Oliver Chang <oychang1@gmail.com>
2012-09-15 14:06:01 -0400

    fixed display issues, made clear screen cross platform, and moved name storage around. cpu_player still kinda wonky.

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,15 @@
-from os import system
+from os import system, name
 from random import shuffle, randrange
+
+def clear_screen():
+    """ 
+    support for clearing screens on multi-platforms
+    http://stackoverflow.com/questions/517970/how-to-clear-python-interpreter-console
+    """
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
 
 def print_board(board):
     """ 
@@ -24,7 +34,7 @@ def print_board(board):
         print "|     " + left + "     |     " + center + "     |     " + right + "     |"
         print "-------------------------------------"
 
-def print_view(turn, board, info):
+def print_view(info, board, turn):
     # print top status bar
     if info[0] == "2": # two humans
         print "Move #%i -- Player 1 is %s, Player 2 is %s" % (turn, info[1], info[2])
@@ -35,13 +45,13 @@ def print_view(turn, board, info):
 
     print_board(board)
                 
-def check_win(turn, ownership):
+def check_win(board, turn, names):
     """ 
-    Given list ownership which is comprised of position strings and ownership strings ('X ' and 'O '), outputs winning piece if there is a winner. 
+    Given list ownership which is comprised of position strings and ownership strings ('X' and 'O'), outputs winning piece if there is a winner. 
     Return true(winner) or false
     """
-    # winning combinations
-    combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    # winning combinations -- refer to cells by their indices
+    combos = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
     xs = []
     os = []
     # have to play 5 rounds before a winner can occur
@@ -49,50 +59,81 @@ def check_win(turn, ownership):
         return False
   
     # generate list of who owns which square
-    for (index, item) in enumerate(ownership):
-        if item == "X ":
+    for (index, item) in enumerate(board):
+        if item == "X":
             xs.append(index)
-        elif item == "O ":
+        elif item == "O":
             os.append(index)
 
-    players = [xs,os]
+    players = (xs, os)
     for combo in combos:
         for player in players:
             # from http://stackoverflow.com/questions/1388818/how-can-i-compare-two-lists-in-python-and-return-matches
             if len(set(player) & set(combo)) == 3: 
                 if player == xs:
-                    print "X Wins!"
+                    print names[0] + " as " + names[1] + " wins!"
                     return True
                 elif player == os:
-                    print "O Wins!"
+                    print names[0] + " as " + names[1] + " wins!"
                     return True
-
-    # elif winner == "scratch" or turn == 10:
-    #     print "no possible winning combination"
-    #     printBoard(board)
                 
-def cell_chooser(player_type, piece, board, turn):
+def cell_chooser(info, board, turn):
     """
     if human, this is easy; we just change value of cell to the player
     elif robot, give to another func
     returns nothing
     """
-    if player_type == "human":
+    gametype, player1_piece, player2_piece = game_info
+
+    if gametype == "2":
+        if (turn % 2 == 1):
+            piece = player1_piece
+            name = "Player 1"
+        else:
+            piece = player2_piece
+            name = "Player 2"
+    # always lets player go first
+    elif gametype == "1":
+        if (turn % 2 == 1):
+            piece = player1_piece
+            name = "Player 1"
+        else:
+            piece = player2_piece
+            name = "CPU 1"
+            cpu_player(board, player2_piece, turn)
+    elif gametype == "c":
+        if (turn % 2 == 1):
+            piece = player1_piece
+            name = "CPU 1"
+            cpu_player(board, player1_piece, turn)
+        else:
+            piece = player2_piece
+            name = "CPU 2"
+            cpu_player(board, player2_piece, turn)
+
+
+    if name[0:3] != "CPU":
         while True:
             chosen_cell = raw_input("%s's turn: " % piece)
             if (chosen_cell in board): # if valid move...
                 # assign current player as the value
                 # not always guaranteed to work if not an int
                 board[int(chosen_cell) - 1] = piece
+                clear_screen()
                 break
             else:
                 print "invalid input/move. say a coordinate e.g. 0"
                 print_board(board)
 
-    else:
-        cpu_player(board, piece, turn)    
+    return (name, piece)
 
 def cpu_player(board, piece, turn):
+    """
+    change board in place with the move of cpu player
+    equivalent to the changes in board made in cell_chooser
+    but with logic to choose the best choice
+    return None
+    """
     # winning combinations
     combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     corners = ["1", "3", "7", "9"]
@@ -103,7 +144,7 @@ def cpu_player(board, piece, turn):
     for (index, item) in enumerate(board):
         if item == piece:
             cpu.append(index)
-        elif (item == "O" or item == "X") and (item != piece):
+        elif item == "O" or item == "X":
             opponent.append(index)
     
     #print cpu #db
@@ -111,53 +152,49 @@ def cpu_player(board, piece, turn):
     
     moved = False
     
+    # detect chance for win
     for combo in combos:
+        # if two in what cpu has and two of a winning solution match...
         if (len(set(cpu) & set(combo)) == 2):
-            #print "CONSIDERING WINNING. POSSIBLE MISSING INDEX:" #db
-            a = (set(combo) - set(cpu))
-            #print a #db
-            if len(a & set(opponent)) == 0: # if two in mine similar to combo and the third one isn't in opponent
-                #print "cpu two similar" #db
-                # get missing -- MUST BE EMPTY      
+            #  and the third one isn't owned by opponent...
+            if len((set(combo) - set(cpu)) & set(opponent)) == 0:    
                 missing = list(set(combo) - set(cpu))[0]
+                print "In an attempt to win... "# db!!!
                 print "CPU as %s plays %s" % (piece, board[missing])
                 board[missing] = piece
-                moved = True
-                break
-    # need to be in 2 for loops or else it will stop when it has the chance to win
-    # if there is the chance to win and the chance to block, it possibly will take the block
-    # this ensures that all the combos are used before going for a block
-    if moved == False:
-        for combo in combos:
-            if len(set(opponent) & set(combo)) == 2: # if two in opponent -- block
-                #print "considering opponent 2 simliar" #db
-                missing = list(set(combo) - set(opponent))[0] # gives a list e.g. [6]
-                print "missing = " + str(missing)
-                if missing not in cpu: # if I don't own missing alraedy
-                    #print "went for it" #db
-                    print "CPU as %s plays %s" % (piece, board[missing])
-                    board[missing] = piece
-                    moved = True
-                    break
-    
-        if (turn < 2) or (set(corners) & set(board) != 0): # if they are still open
-            #print "corner move" #db
-            # get the string of the remaining square in board
-            # pairs in corners that are in board
-            #print "TAKING [0] OF THIS LIST" #db
-            #print list(set(corners) & set(board)) #db
-            missing = list(set(corners) & set(board))[0]
-            print "CPU as %s plays %s" % (piece, board[board.index(missing)])
-            board[board.index(missing)] = piece
-        else:
-            #print "naive method" # debug
-            # naive cpu -- chooses a random unasigned square
-            while True:
-                randomInd = board[randrange(0,9)]
-                if (randomInd != "X") and (randomInd != "O"):
-                    print "CPU as %s plays %s" % (piece, randomInd)
-                    board[board.index(randomInd)] = piece
-                    break
+                return None
+    # vvvvv don't really understand why this is vvvvv
+    # need to be in 2 for loops or else it will stop when it has the chance to win. if there is the chance to win and the chance to block, it possibly will take the block. this ensures that all the combos are used before going for a block
+
+    # detect chance to block: if opponent has two of a winning combo
+    for combo in combos:
+        if len(set(opponent) & set(combo)) == 2:
+            # gives a list e.g. [6]
+            missing = list(set(combo) - set(opponent))[0] 
+            print "index of cell opponent is missing = " + str(missing)
+            # if I don't own missing already, block
+            if missing not in cpu:
+                print "To block..."
+                print "CPU as %s plays %s" % (piece, board[missing])
+                board[missing] = piece
+                return None
+
+    # if turn less than 2 or corners still open...
+    if (turn < 2) or (set(corners) & set(board) != 0):
+        # pick an untaken corner cell
+        missing = list(set(corners) & set(board))[0]
+        print "CPU loves corners and so..."
+        print "CPU as %s plays %s" % (piece, board[board.index(missing)])
+        board[board.index(missing)] = piece
+    # naive cpu -- chooses a random unasigned square
+    else: 
+        while True:
+            randomInd = board[randrange(0,9)]
+            if (randomInd != "X") and (randomInd != "O"):
+                print "In an act of desparation..."
+                print "CPU as %s plays %s" % (piece, randomInd)
+                board[board.index(randomInd)] = piece
+                return
 
 def setup_players():
     """
@@ -183,45 +220,26 @@ def setup_players():
 
 if __name__ == "__main__":
     # setup
+    id_info = ("", "")
     turn = 1
     board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    system("cls")
+    clear_screen()
     print "Tic-Tac-Toe Program\n"
     # split into two steps so that we can easily pass all this info to print
     game_info = setup_players()
-    system('cls')
-    gametype, player1, player2 = game_info
-    # maximum amount of moves in a tictactoe game is 9
-    while turn <= 10 and turn >= 1: 
-        if check_win(turn, board):
-            break
-            
-        print_view(turn, board, game_info)
+    clear_screen()
 
-        if gametype == "2":
-             # if turn is even...
-            if (turn % 2 == 1):
-                cell_chooser("human", player1, board, turn)
-                system('cls') # clear screen -- windows only!
-            # else if turn is odd...
-            else: 
-                cell_chooser("human", player2, board, turn)
-                system('cls') # clear screen -- windows only!
-        elif gametype == "1":
-            if (turn % 2 == 1):
-                cell_chooser("human", player1, board, turn)
-                system('cls') # clear screen -- windows only!
-            else:
-                cell_chooser("cpu", player2, board, turn)
-        elif gametype == "c":
-            if (turn % 2 == 1):
-                cell_chooser("cpu", player1, board, turn)
-            else:
-                cell_chooser("cpu", player2, board, turn)
+    # maximum amount of moves in a tictactoe game is 9
+    while turn <= 10 and turn >= 1:
+        print_view(game_info, board, turn)
+
+        if check_win(board, turn, id_info):
+            break
+
+        id_info = cell_chooser(game_info, board, turn)
 
         turn += 1
-
 
 
     raise SystemExit(0)
